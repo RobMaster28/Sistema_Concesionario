65d5e2275a70dc354e7fba791be2436594fa43cb
RobMaster28 <180604094+RobMaster28@users.noreply.github.com>
2025-07-18 17:03:07 -0500

    initial commit

---
+++ src/main/java/org/example/api_concesionario/ApiConcesionarioApplication.java
@@ -0,0 +1,13 @@
+package org.example.api_concesionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConcesionarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiConcesionarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/api_concesionario/ApiConcesionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.api_concesionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConcesionarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
